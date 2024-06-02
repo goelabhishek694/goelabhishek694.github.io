bacfe0b1557a9423016fea8dc6214615f4ad590a
Abhishek <abhishekgoel@gofynd.com>
2024-06-02 16:49:00 +0530

    test

--- js/about.js
+++ js/about.js
@@ -1,32 +1,53 @@
 (function () {
-  $(".flex-container-mission").waitForImages(
-    function () {
-      $(".spinner-mission").fadeOut();
-    },
-    $.noop,
-    true
-  );
+  // Wait for images to load
+  var images = document.querySelectorAll(".flex-container-mission img");
+  var loadedImagesCount = 0;
+  var totalImages = images.length;
+  
+  function imageLoaded() {
+    loadedImagesCount++;
+    if (loadedImagesCount === totalImages) {
+      document.querySelector(".spinner-mission").style.display = 'none';
+    }
+  }
 
-  $(".flex-slide-mission").each(function () {
-    $(this).hover(
-      function () {
-        $(this).find(".flex-title-mission").css({
-          transform: "rotate(0deg)",
-          top: "10%",
-        });
-        $(this).find(".flex-about-mission").css({
-          opacity: "1",
-        });
-      },
-      function () {
-        $(this).find(".flex-title-mission").css({
-          transform: "rotate(90deg)",
-          top: "15%",
-        });
-        $(this).find(".flex-about-mission").css({
-          opacity: "0",
-        });
+  images.forEach(function (img) {
+    if (img.complete) {
+      imageLoaded();
+    } else {
+      img.addEventListener('load', imageLoaded);
+      img.addEventListener('error', imageLoaded);
+    }
+  });
+
+  // Hover effect for flex slides
+  var flexSlides = document.querySelectorAll(".flex-slide-mission");
+
+  flexSlides.forEach(function (slide) {
+    slide.addEventListener('mouseenter', function () {
+      var title = slide.querySelector(".flex-title-mission");
+      var about = slide.querySelector(".flex-about-mission");
+
+      if (title) {
+        title.style.transform = "rotate(0deg)";
+        title.style.top = "10%";
       }
-    );
+      if (about) {
+        about.style.opacity = "1";
+      }
+    });
+
+    slide.addEventListener('mouseleave', function () {
+      var title = slide.querySelector(".flex-title-mission");
+      var about = slide.querySelector(".flex-about-mission");
+
+      if (title) {
+        title.style.transform = "rotate(90deg)";
+        title.style.top = "15%";
+      }
+      if (about) {
+        about.style.opacity = "0";
+      }
+    });
   });
 })();
